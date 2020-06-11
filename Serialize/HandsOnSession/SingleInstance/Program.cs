5c04186c95c457394203915eac22e2ce34629741
Farago Balazs <balage.farago@gmail.com>
2020-06-11 19:41:41 +0200

    Threading exercise 2 completed

---
+++ Serialize/HandsOnSession/SingleInstance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = null;
+            const string name = "RUNMEONLYONCE";
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Checking for mutex with name:" + name);
+                    mutex = Mutex.OpenExisting(name);
+                    mutex.Close();
+                    Console.WriteLine("Exiting...");
+                    Console.ReadKey();
+                    System.Environment.Exit(0);
+                }
+                catch (WaitHandleCannotBeOpenedException e)
+                {
+                    mutex = new Mutex(false, name);
+                    Console.WriteLine("Initialize mutex local variable");
+                }
+            }
+        }
+    }
+}
